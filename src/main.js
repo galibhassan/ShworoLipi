f1b5fba374654fed10d440204b6a4d6402c7eb80
Galib Hassan <Galib.Hassan@autodesk.com>
2018-05-07 18:13:45 +0200

    tonic selector added, song class to generate absolute notes from user input connected to main.js, a warning-div added for status

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
+var tonic = 'C3';
 var inputTextAnalyzer = require('./inputTextAnalyzer');
+var Song = require('./song');
 
 window.addEventListener('load', function () {
-
-
+  var tonicSelector = document.getElementById("tonic");
+  tonicSelector.addEventListener("change", function(){
+    tonic = tonicSelector.value;
+    console.log(tonic);
+  });
+  
   var mainButton = document.getElementById("mainButton");
   mainButton.addEventListener("mousedown", function () {
     var userInputNotes = document.getElementById("userInputNotes").innerText;
-    var amarGaan = inputTextAnalyzer.generateThings(userInputNotes);
-    console.log(amarGaan);
+    var shworolipi = inputTextAnalyzer.generateThings(userInputNotes);
+    var song = new Song(shworolipi.noteList, tonic);
+
+    console.log(song.getAbsoluteNotes());
   });
 });
